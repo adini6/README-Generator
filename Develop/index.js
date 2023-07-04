b033f264bb0ee92449fef3d9c081bb274e92d7f3
Abdikalik Dini <adini18@gmail.com>
2023-07-03 20:37:41 -0400

    added array questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
-// TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+//packages needed for this application
+const inquirer = require('inquirer');
+// an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter the title of your project:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe the purpose and functionality of the project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide installation instructions:',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Explain how to use the project:',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Choose a license for your project:',
+      choices: ['MIT', 'Apache 2.0', 'GPLv2', 'Other', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Explain how others can contribute to the project:',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Provide instructions on running tests for the project:',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Enter your email address:',
+    },
+  ];
+  
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
