cb1e8708b8e840260e44dc7c35b0a825c3741e14
Abdikalik Dini <adini18@gmail.com>
2023-07-05 09:25:45 -0400

    added function that returns license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,19 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    if (license === "MIT") {
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    } else if (license === "Apache 2.0") {
+      return "![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    } else if (license === "GPLv2") {
+      return "![License: GPLv2](https://img.shields.io/badge/License-GPLv2-blue.svg)";
+    } else if (license === "Other") {
+      return "![License: Other](https://img.shields.io/badge/License-Other-lightgrey.svg)";
+    }
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
